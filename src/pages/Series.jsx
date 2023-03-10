d30e7282768d1403b864a39a4172661189c50b5d
virginia-zuror <virginiazuor@gmail.com>
2023-03-10 22:31:10 +0100

    fetching...

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,15 +1,39 @@
 import './Series.css';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Series = () => {
+  const [all, setAll] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [series, setSeries] = useState([]);
+
+  const getAll = async () => {
+    // eslint-disable-next-line prettier/prettier
+    fetch('https://raw.githubusercontent.com/StreamCo/react-coding-challenge/master/feed/sample.json')
+      .then((res) => res.json())
+      .then((res) => setAll(res.entries));
+
+    setLoaded(true);
+  };
+  useEffect(() => {
+    getAll();
+  }, []);
+
   return (
     <div>
       <div className="title_series">
         <h3>Popular Series</h3>
       </div>
       <div className="content">
-        <figure className="figure_series">Popular Series</figure>
+        {!loaded ? (
+          <h3>Loading...</h3>
+        ) : all.length > 0 ? (
+          all.map((show) => {
+            show.programType.includes('series') && <h3 key={show.title}>{show.title}</h3>;
+          })
+        ) : (
+          <h3>Error...</h3>
+        )}
       </div>
     </div>
   );
